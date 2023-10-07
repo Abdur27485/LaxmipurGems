4289280f57ba9479d30908aeb703bacc2dff0765
Abdur Rahman <abdurrahman27485@gmail.com>
2023-10-08 05:48:29 +0600

    react router setup

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../../component/Footer/Footer';
 import Navbar from '../../component/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Outlet></Outlet>
             <Footer></Footer>
         </div>
     );
