904cbe42e273f753c262c135d12240b91e5245a8
Abdur Rahman <abdurrahman27485@gmail.com>
2023-10-09 21:32:32 +0600

    all jewellery, add jewellery, blogs

---
+++ src/pages/Add Jewellery/AddJewellery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddJewellery = () => {
+    return (
+        <div>
+            <h1>this is add Jewellery</h1>
+        </div>
+    );
+};
+
+export default AddJewellery;
---
+++ src/pages/All jewellery/AllJewellery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllJewellery = () => {
+    return (
+        <div>
+            <h1>This is all AllJewellery</h1>
+        </div>
+    );
+};
+
+export default AllJewellery;
