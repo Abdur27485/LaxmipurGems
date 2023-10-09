17c0f507c38d286733fc5f6daf0bd202cb9d6b60
Abdur Rahman <abdurrahman27485@gmail.com>
2023-10-09 18:57:38 +0600

    imgBB upload successful

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,33 +1,43 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Firebase/AuthProvider';
+import { useForm } from 'react-hook-form';
 
 const SignUp = () => {
     const [showPassword, SetShowPassword] = useState(false)
+    const { register, handleSubmit } = useForm();
 
     const imgUploadToken = import.meta.env.VITE_imgBBToken;
     const imgHostingUrl = `https://api.imgbb.com/1/upload?key=${imgUploadToken}`;
     console.log(imgUploadToken)
 
     const { createUser } = useContext(AuthContext)
-    const signUpHandler = (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        const confirmPassword = form.confirmPassword.value;
+    const signUpHandler = data => {
+        console.log(data)
+
+        const formImg = new FormData();
+        formImg.append('image', data.profileImg[0])
+
+        fetch(imgHostingUrl, {
+            method: 'POST',
+            body: formImg
+        })
+        .then(res => res.json())
+        .then( imgUrl =>{
+            console.log(imgUrl)
+        })
 
     }
     return (
         <div>
             <div className='mt-[40px] mb-[80px]'>
                 <h1 className='text-center text-5xl mb-5'>Create Account</h1>
-                <form onSubmit={signUpHandler} className='mx-auto max-w-xs border border-[#590d82] rounded px-5 py-5'>
+                <form onSubmit={handleSubmit(signUpHandler)} className='mx-auto max-w-xs border border-[#590d82] rounded px-5 py-5'>
                     <div className="form-control w-full">
                         <label className="label">
                             <span className="label-text font-semibold">Your Email</span>
                         </label>
-                        <input name='email' type="email" className="file-input file-input-bordered border-[#FEACC6] w-full max-w-xs px-2 mb-5" placeholder='Enter your email' required />
+                        <input name='email' {...register('email')} type="email" className="file-input file-input-bordered border-[#FEACC6] w-full max-w-xs px-2 mb-5" placeholder='Enter your email' required />
                     </div>
                     <div className="form-control w-full mb-5">
                         <label className="label">
@@ -45,7 +55,7 @@ const SignUp = () => {
                         <label className="label">
                             <span className="label-text font-semibold">Profile Picture</span>
                         </label>
-                        <input type="file" name='profilePic' className="file-input file-input-bordered w-full max-w-xs border-[#FEACC6]" required />
+                        <input type="file" {...register('profileImg')} className="file-input file-input-bordered w-full max-w-xs border-[#FEACC6]" required />
                     </div>
                     <button type="submit" className='w-full mb-3 bg-[#7912b0] hover:bg-[#9816df] py-2 px-5 rounded font-semibold text-white'>Sign Up to Continue</button>
                     <div className=''>
